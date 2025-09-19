cf9569bd3db5a5fe5f20ccab662a3f8b5a49f6c0
oncord <meyer.r.l@wustl.edu>
2025-09-19 16:12:32 -0500

    studio3

---
+++ studio3/studio2.cpp
@@ -0,0 +1,95 @@
+/*
+ *  Richard Meyer
+ *
+ *  studio2.cpp
+ */
+
+#include "studio2.h"
+#include <iostream>
+
+std::ostream& operator<<(std::ostream& os, Weekday& w)
+{
+    switch (w)
+    {
+        case Weekday::sun:
+            os << "Sunday" << " ";
+            break;
+        case Weekday::mon:
+            os << "Monday" << " ";
+            break;
+        case Weekday::tue:
+            os << "Tuesday" << " ";
+            break;
+        case Weekday::wed:
+            os << "Wednesday" << " ";
+            break;
+        case Weekday::thu:
+            os << "Thusday" << " ";
+            break;
+        case Weekday::fri:
+            os << "Friday" << " ";
+            break;
+        case Weekday::sat:
+            os << "Saturday" << " ";
+            break;
+        default:
+            os << "Undefined" << " ";
+            break;
+    };
+
+    return os;
+}
+
+Weekday& operator++(Weekday& w)
+{
+    switch (w)
+    {
+        case Weekday::sun:
+            w = Weekday::mon;
+            break;
+        case Weekday::mon:
+            w = Weekday::tue;
+            break;
+        case Weekday::tue:
+            w = Weekday::wed;
+            break;
+        case Weekday::wed:
+            w = Weekday::thu;
+            break;
+        case Weekday::thu:
+            w = Weekday::fri;
+            break;
+        case Weekday::fri:
+            w = Weekday::sat;
+            break;
+        case Weekday::sat:
+            w=  Weekday::sun;
+            break;
+        case Weekday::sun:
+            w = Weekday::mon;
+            break;
+    };
+
+    return w;
+}
+
+Pressure& operator++(Pressure& p, int)
+{
+    switch (p)
+    {
+        case Pressure::lo:
+            p = Pressure::med;
+            break;
+        case Pressure::med:
+            p = Pressure::hi;
+            break;
+        case Pressure::hi:
+            p = Pressure::pop;
+            break;
+        case Pressure::pop:
+            break;
+            
+    }
+
+    return p;
+}
