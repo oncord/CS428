3c5cf69c6197f63b903175dbc628ac8ac6418b61
oncord <meyer.r.l@wustl.edu>
2025-09-23 12:00:24 -0500

    studio5

---
+++ studio5/build/CMakeFiles/studio5.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/studio5.dir/main.cpp.o"
+  "CMakeFiles/studio5.dir/main.cpp.o.d"
+  "CMakeFiles/studio5.dir/studio5.cpp.o"
+  "CMakeFiles/studio5.dir/studio5.cpp.o.d"
+  "studio5"
+  "studio5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/studio5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ studio5/build/CMakeFiles/studio5.dir/studio5.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/studio5.dir/studio5.cpp.o: \
+ /home/compute/meyer.r.l/428/studio5/studio5.cpp \
+ /usr/include/stdc-predef.h
